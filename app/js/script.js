264b71ccf0af80d25f1f1eb5e43b2f6564ea9bb8
Blurryface1998 <83365791+Blurryface1998@users.noreply.github.com>
2022-08-09 08:42:54 +0200

    Bugs Fixed
    Added message to show when email input is correct. added transition when email input is invalid or incorrect. Changed some of the SCSS

--- app/js/script.js
+++ app/js/script.js
@@ -2,25 +2,36 @@ const inputE = document.getElementById('email');
 const submit = document.getElementById('submit');
 const iconError = document.getElementById('icon-error');
 const errorText = document.getElementById('text-error');
-const emailFormat = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+const emailFormat = "([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])";
 const form = document.getElementById('form');
 let error = [];
 
 submit.addEventListener("click", e => {
     error = [];
-    e.preventDefault();
     const email = inputE.value;
     
     if (email == "" || email === undefined) {
         error.push('The "input" field is emty');
         iconError.style.display = "block";
         errorText.innerHTML = error[0];
-        form.style.border= "1px solid var(--softRed)";
+        errorText.style.color = "var(--softRed)";
+        errorText.style.display = "block";
+        form.style.border= "2px solid var(--softRed)";
         
     } else if (!email.match(emailFormat)) {
         error.push("The email address is not formatted correctly");
         iconError.style.display = "block";
         errorText.innerHTML = error[0];
-        form.style.border= "1px solid var(--softRed)";
+        errorText.style.color = "var(--softRed)";
+        errorText.style.display = "block";
+        form.style.border= "2px solid var(--softRed)";
+    } else {
+        error.push("Thank You!");
+        iconError.style.display = "none";
+        errorText.innerHTML = error[0];
+        errorText.style.color = "blue";
+        errorText.style.display = "block";
+        form.style.border = "1px solid var(--desaturatedRed)";
     }
+    e.preventDefault();
 });
